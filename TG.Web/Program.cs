62817fbde05ec01bc0cafe8b36869704743721d7
Tomáš <tomasmike182@gmail.com>
2021-01-14 07:46:11 +0100

    .

--- TG.Web/Program.cs
+++ TG.Web/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -13,7 +14,16 @@ namespace TG.Web
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = new WebHostBuilder()
+          .UseKestrel()
+          .UseContentRoot(Directory.GetCurrentDirectory())
+          .UseIISIntegration()
+          .UseStartup(@"E:\Code\TG\TG.Web\web")
+          .Build();
+
+            host.Run();
+
+            //CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
